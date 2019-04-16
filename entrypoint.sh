f3bd6dc3237ff103c704383d4948ed9b06a9f262
Neri Marschik <codesuki@users.noreply.github.com>
2019-04-16 11:10:14 +0900

    fix: shellcheck issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,9 @@ main () {
     BRANCH=$2 #$(git rev-parse --abbrev-ref HEAD)
     PATH_REGEX=$3
 
-    PR_CHANGED_PATHS=$(git diff --name-only $(git merge-base $BASE $BRANCH) $BRANCH | grep -o -P "$PATH_REGEX" | uniq | sort)
+    PR_CHANGED_PATHS=$(git diff --name-only "$(git merge-base "$BASE" "$BRANCH")" "$BRANCH" | grep -o -P "$PATH_REGEX" | uniq | sort)
 
-    MASTER_CHANGED_PATHS=$(git diff --name-only $(git merge-base $BASE $BRANCH) $BASE | grep -o -P "$PATH_REGEX" | uniq | sort)
+    MASTER_CHANGED_PATHS=$(git diff --name-only "$(git merge-base "$BASE" "$BRANCH")" "$BASE" | grep -o -P "$PATH_REGEX" | uniq | sort)
 
     BOTH_CHANGED_PATHS=$(comm -12 <(echo "$PR_CHANGED_PATHS") <(echo "$MASTER_CHANGED_PATHS"))
 
@@ -18,7 +18,7 @@ main () {
         # OK TO MERGE
         exit 0
     else
-        echo $BOTH_CHANGED_PATHS
+        echo "$BOTH_CHANGED_PATHS"
         exit 1
     fi
 }
